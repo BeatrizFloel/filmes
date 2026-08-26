15f92484ce8f9c44484baf256691ec67f2b78878
Beatriz Floel <beatriz.floel@eaportal.org>
2026-08-26 11:39:15 -0300

    blabla

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ const sql = mysql2.createPool({
 });
 
 
+app.get("/", (req, res) => {
 
-app.get("/filmes", (req, res) => {
+    const c = "SELECT * FROM filmes_BeatrizFloelGiovannaMigliorini";
 
-    const sql = "SELECT * FROM filmes_BeatrizFloelGiovannaMigliorini";
-
-    sql.query(sql, (erro, resultado) => {
+    sql.query(c, (erro, resultado) => {
         if (erro) {
             return res.status(500).json(erro);
         }
@@ -34,9 +33,9 @@ app.get("/filmes", (req, res) => {
 
 app.get("/filmes/:id", (req, res) => {
 
-    const sql = "SELECT * FROM filmes_BeatrizFloelGiovannaMigliorini WHERE id = ?";
+    const b = "SELECT * FROM filmes_BeatrizFloelGiovannaMigliorini WHERE id = ?";
 
-    sql.query(sql, [req.params.id], (erro, resultado) => {
+    sql.query(b, [req.params.id], (erro, resultado) => {
         if (erro) {
             return res.status(500).json(erro);
         }
@@ -51,13 +50,13 @@ app.post("/filmes", (req, res) => {
 
     const { title, genre, duration, age_rating } = req.body;
 
-    const sql = `
+    const a = `
         INSERT INTO filmes_BeatrizFloelGiovannaMigliorini
         (title, genre, duration, age_rating)
         VALUES (?, ?, ?, ?)
     `;
 
-    sql.query(sql, [title, genre, duration, age_rating], (erro) => {
+    sql.query(a, [title, genre, duration, age_rating], (erro) => {
         if (erro) {
             return res.status(500).json(erro);
         }
@@ -72,14 +71,14 @@ app.put("/filmes/:id", (req, res) => {
 
     const { title, genre, duration, age_rating } = req.body;
 
-    const sql = `
+    const d = `
         UPDATE filmes_BeatrizFloelGiovannaMigliorini
         SET title = ?, genre = ?, duration = ?, age_rating = ?
         WHERE id = ?
     `;
 
     sql.query(
-        sql,
+        d,
         [title, genre, duration, age_rating, req.params.id],
         (erro) => {
             if (erro) {
@@ -94,9 +93,9 @@ app.put("/filmes/:id", (req, res) => {
 
 app.delete("/filmes/:id", (req, res) => {                                     
 
-    const sql = "DELETE FROM filmes_BeatrizFloelGiovannaMigliorini WHERE id = ?";
+    const f = "DELETE FROM filmes_BeatrizFloelGiovannaMigliorini WHERE id = ?";
 
-    sql.query(sql, [req.params.id], (erro) => {
+    sql.query(f, [req.params.id], (erro) => {
         if (erro) {
             return res.status(500).json(erro);
         }
